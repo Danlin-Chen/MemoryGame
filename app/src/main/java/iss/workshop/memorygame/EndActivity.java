53fd018877ef746836b33da09dc5644906402b9e
kyawmt <kmt@kmtkko.com>
2021-07-16 21:59:32 +0800

    Fix the bug in screen rotation in EndActivity

--- app/src/main/java/iss/workshop/memorygame/EndActivity.java
+++ app/src/main/java/iss/workshop/memorygame/EndActivity.java
@@ -51,90 +51,106 @@ public class EndActivity extends AppCompatActivity {
         bName3 = pref.getString("bName3", null);
         player = pref.getString("player", null);
 
-        if(score < best1) {
-            long temp = best1;
-            best1 = score;
-            long temp2 = best2;
-            best2 = temp;
-            best3 = temp2;
-
-            String sTemp = bName1;
-            bName1 = player;
-            String sTemp2 = bName2;
-            bName2 = sTemp;
-            bName3 = sTemp2;
-
-            editor.putLong("best3", best3);
-            editor.putLong("best2", best2);
-            editor.putLong("best1", best1);
-            editor.putString("bName1", bName1);
-            editor.putString("bName2", bName2);
-            editor.putString("bName3", bName3);
-            editor.commit();
-            updatedFlag = true;
-        }
-        else if(score < best2) {
-            Long temp = best2;
-            best2 = score;
-            best3 = temp;
-
-            String sTemp = bName2;
-            bName2 = player;
-            bName3 = sTemp;
-
-            editor.putLong("best3", best3);
-            editor.putLong("best2", best2);
-            editor.putString("bName2", bName2);
-            editor.putString("bName3", bName3);
-            editor.commit();
-            updatedFlag = true;
-        }
-        else if(score < best3) {
-            best3 = score;
-            editor.putLong("best3", best3);
-            editor.putString("bName3", player);
-            editor.commit();
-            updatedFlag = true;
-        }
-
         mScoreChart.setText(getString(R.string.score2_vertical,conversionOfTime(score)));
 
-        if(best1 == 0 && updatedFlag == false) {
-            mBestOne.setText(getString(R.string.leader1,player, conversionOfTime(score)));
-            editor.putString("bName1", player);
-            editor.putLong("best1", score);
-            editor.commit();
-        }
-        else if (best2 == 0 && updatedFlag == false) {
-            mBestOne.setText(getString(R.string.leader1,bName1,conversionOfTime(best1)));
-            mBestTwo.setText(getString(R.string.leader1,player,conversionOfTime(score)));
-            editor.putString("bName2", player);
-            editor.putLong("best2", score);
-            editor.commit();
-        }
-        else if (best3 == 0 && updatedFlag == false) {
-            mBestOne.setText(getString(R.string.leader1,bName1,conversionOfTime(best1)));
-            mBestTwo.setText(getString(R.string.leader1,bName2,conversionOfTime(best2)));
-            mBestThree.setText(getString(R.string.leader1,player,conversionOfTime(score)));
-            editor.putString("bName3", player);
-            editor.putLong("best3", score);
-            editor.commit();
-        }
-        else {
-            if(best3 == 0) {
-                mBestOne.setText(getString(R.string.leader1,bName1,conversionOfTime(best1)));
-                mBestTwo.setText(getString(R.string.leader1,bName2,conversionOfTime(best2)));
-            }
-            else if(best2 == 0 && best3 == 0) {
-                mBestOne.setText(getString(R.string.leader1,bName1,conversionOfTime(best1)));
-            }
-            else {
+        if(savedInstanceState!=null){
+
+            if(bName1!=null){
                 mBestOne.setText(getString(R.string.leader1, bName1, conversionOfTime(best1)));
+            }
+
+            if(bName2!=null){
                 mBestTwo.setText(getString(R.string.leader1, bName2, conversionOfTime(best2)));
+            }
+
+            if(bName3!=null){
                 mBestThree.setText(getString(R.string.leader1, bName3, conversionOfTime(best3)));
             }
-        }
 
+        }else{
+
+            if(score < best1) {
+                long temp = best1;
+                best1 = score;
+                long temp2 = best2;
+                best2 = temp;
+                best3 = temp2;
+
+                String sTemp = bName1;
+                bName1 = player;
+                String sTemp2 = bName2;
+                bName2 = sTemp;
+                bName3 = sTemp2;
+
+                editor.putLong("best3", best3);
+                editor.putLong("best2", best2);
+                editor.putLong("best1", best1);
+                editor.putString("bName1", bName1);
+                editor.putString("bName2", bName2);
+                editor.putString("bName3", bName3);
+                editor.commit();
+                updatedFlag = true;
+            }
+            else if(score < best2) {
+                Long temp = best2;
+                best2 = score;
+                best3 = temp;
+
+                String sTemp = bName2;
+                bName2 = player;
+                bName3 = sTemp;
+
+                editor.putLong("best3", best3);
+                editor.putLong("best2", best2);
+                editor.putString("bName2", bName2);
+                editor.putString("bName3", bName3);
+                editor.commit();
+                updatedFlag = true;
+            }
+            else if(score < best3) {
+                best3 = score;
+                editor.putLong("best3", best3);
+                editor.putString("bName3", player);
+                editor.commit();
+                updatedFlag = true;
+            }
+
+            if(best1 == 0 && updatedFlag == false) {
+                mBestOne.setText(getString(R.string.leader1,player, conversionOfTime(score)));
+                editor.putString("bName1", player);
+                editor.putLong("best1", score);
+                editor.commit();
+            }
+            else if (best2 == 0 && updatedFlag == false) {
+                mBestOne.setText(getString(R.string.leader1,bName1,conversionOfTime(best1)));
+                mBestTwo.setText(getString(R.string.leader1,player,conversionOfTime(score)));
+                editor.putString("bName2", player);
+                editor.putLong("best2", score);
+                editor.commit();
+            }
+            else if (best3 == 0 && updatedFlag == false) {
+                mBestOne.setText(getString(R.string.leader1,bName1,conversionOfTime(best1)));
+                mBestTwo.setText(getString(R.string.leader1,bName2,conversionOfTime(best2)));
+                mBestThree.setText(getString(R.string.leader1,player,conversionOfTime(score)));
+                editor.putString("bName3", player);
+                editor.putLong("best3", score);
+                editor.commit();
+            }
+            else {
+                if(best3 == 0) {
+                    mBestOne.setText(getString(R.string.leader1,bName1,conversionOfTime(best1)));
+                    mBestTwo.setText(getString(R.string.leader1,bName2,conversionOfTime(best2)));
+                }
+                else if(best2 == 0 && best3 == 0) {
+                    mBestOne.setText(getString(R.string.leader1,bName1,conversionOfTime(best1)));
+                }
+                else {
+                    mBestOne.setText(getString(R.string.leader1, bName1, conversionOfTime(best1)));
+                    mBestTwo.setText(getString(R.string.leader1, bName2, conversionOfTime(best2)));
+                    mBestThree.setText(getString(R.string.leader1, bName3, conversionOfTime(best3)));
+                }
+            }
+        }
 
         mPlayAgainBtn.setOnClickListener(new View.OnClickListener() {
             @Override
